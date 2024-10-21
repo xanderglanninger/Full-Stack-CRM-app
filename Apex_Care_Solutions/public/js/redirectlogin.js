8b8f80ef66abe114692c168d3d4774bf2c96778c
Xander Glanninger <141350991+xanderglanninger@users.noreply.github.com>
2024-10-21 22:37:18 +0200

    Delete Apex_Care_Solutions directory

--- Apex_Care_Solutions/public/js/redirectlogin.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("login-button").addEventListener("click", function () {
-    window.location.href = "/dashboard"; // Redirect to dashboard page
-  });
-});
