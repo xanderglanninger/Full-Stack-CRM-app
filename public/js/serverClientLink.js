84bda298fd0cd0c780e51c52eb5180200412939e
Xander Glanninger <141350991+xanderglanninger@users.noreply.github.com>
2024-10-24 12:24:42 +0200

    Update serverClientLink.js

--- public/js/serverClientLink.js
+++ public/js/serverClientLink.js
@@ -12,7 +12,7 @@ const GoogleAI = () => {
 };
 
 function redirectToPage() {
-  window.location.href = "http://localhost:5000/loadingscreen";
+  window.location.href = "https://apexcaresolutions-5d1fg4vm.b4a.run//loadingscreen";
 }
 
 // Assuming this is part of your fetch call
